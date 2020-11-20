af7ac505e37b78895e8f8c34a7b8176099dd6d81
Kevin Cloud <kevinmichaelcloud@gmail.com>
2020-11-20 13:38:03 -0800

    First pass to add values for killing convert processes

--- kill_pid_after_time.php
+++ kill_pid_after_time.php
@@ -30,72 +30,145 @@ if ($result = $conn->query("SELECT DATABASE()")) {
     $result->close();
 }
 
-$command = 'ps -C tesseract -F';
-$output = $ret = array();
-$call = exec($command, $output, $ret);
+kill_tesseract_jobs_after_time()
+kill_convert_jobs_after_time()
 
-$keys = array();
-foreach ($output as $i => $process_line) {
-  $process_line = str_replace("  ", " ", $process_line);
-  $process_line = str_replace("  ", " ", $process_line);
-  $process_line = str_replace("  ", " ", $process_line);
-  $process_line = str_replace("  ", " ", $process_line);
-  $process_line = str_replace("  ", " ", $process_line);
-  $output[$i] = $process_line;
-  $parts = explode(" ", $process_line);
-  if (count($keys) < 1) {
-    $keys = explode(" ", $process_line);
-  }
-}
+  function kill_tesseract_jobs_after_time(){
+    # reworked this block for convert tasks
+    $command = 'ps -C tesseract -F';
+    $output = $ret = array();
+    $call = exec($command, $output, $ret);
 
-$pid_index = array_search('PID', $keys);
-$time_index = array_search('TIME', $keys);
-
-$first_line = TRUE;
-$run_output = array();
-foreach ($output as $i => $process_line) {
-  if ($first_line) {
-    $first_line = FALSE;
-  }
-  else {
-    $parts = explode(" ", $process_line);
-
-    $this_pid = $parts[$pid_index];
-    $this_time = $parts[$time_index];
-    @list($h, $m, $s) = explode(":", $this_time);
-    $time_in_seconds = ($h * 3600) + ($m * 60) + $s;
-
-    $tesseract_index= array_search('/usr/local/bin/tesseract', $parts);
-    $this_payload = $parts[($tesseract_index + 1)];
-    $this_payload = str_replace(
-      array('pitt_', '_OBJ.tif', '/tmp/'),
-      array('pitt:', '', ''),
-      $this_payload);
-
-    // Check to see if this line exceeds the threshold.
-    if ($time_in_seconds > TIME_THRESHOLD) {
-      $markup = kill_pid_and_remove_job($this_pid, $this_payload, $conn, $time_in_seconds);
-      if ($markup) {
-        $run_output[] = $markup;
+    $keys = array();
+    foreach ($output as $i => $process_line) {
+      $process_line = str_replace("  ", " ", $process_line);
+      $process_line = str_replace("  ", " ", $process_line);
+      $process_line = str_replace("  ", " ", $process_line);
+      $process_line = str_replace("  ", " ", $process_line);
+      $process_line = str_replace("  ", " ", $process_line);
+      $output[$i] = $process_line;
+      $parts = explode(" ", $process_line);
+      if (count($keys) < 1) {
+        $keys = explode(" ", $process_line);
       }
     }
+
+    $pid_index = array_search('PID', $keys);
+    $time_index = array_search('TIME', $keys);
+
+    $first_line = TRUE;
+    $run_output = array();
+    foreach ($output as $i => $process_line) {
+      if ($first_line) {
+        $first_line = FALSE;
+      }
+      else {
+        $parts = explode(" ", $process_line);
+
+        $this_pid = $parts[$pid_index];
+        $this_time = $parts[$time_index];
+        @list($h, $m, $s) = explode(":", $this_time);
+        $time_in_seconds = ($h * 3600) + ($m * 60) + $s;
+
+        $tesseract_index= array_search('/usr/local/bin/tesseract', $parts);
+        $this_payload = $parts[($tesseract_index + 1)];
+        $this_payload = str_replace(
+          array('pitt_', '_OBJ.tif', '/tmp/'),
+          array('pitt:', '', ''),
+          $this_payload);
+
+        // Check to see if this line exceeds the threshold.
+        if ($time_in_seconds > TIME_THRESHOLD) {
+          $markup = kill_pid_and_remove_ocr_job($this_pid, $this_payload, $conn, $time_in_seconds);
+          if ($markup) {
+            $run_output[] = $markup;
+          }
+        }
+      }
+    }
+    @mysqli_close($conn);
+
+    if (count($run_output) > 0) {
+      echo "The CRON job on " . $server_name . " /opt/islandora_cron/kill_pid_after_time.php is killing some OCR jobs because they exceeded the threshold of " . TIME_THRESHOLD . " seconds.\n\n";
+      foreach ($run_output as $pid_output) {
+        echo $pid_output;
+      }
+      exit(1);
+    }
+    else {
+      exit(0);
+    }
   }
-}
-@mysqli_close($conn);
 
-if (count($run_output) > 0) {
-  echo "The CRON job on " . $server_name . " /opt/islandora_cron/kill_pid_after_time.php is killing some OCR jobs because they exceeded the theshold of " . TIME_THRESHOLD . " seconds.\n\n";
-  foreach ($run_output as $pid_output) {
-    echo $pid_output;
+  function kill_convert_jobs_after_time(){
+    # reworked this block for convert tasks
+    $command = 'ps -C convert -F';
+    $output = $ret = array();
+    $call = exec($command, $output, $ret);
+
+    $keys = array();
+    foreach ($output as $i => $process_line) {
+      $process_line = str_replace("  ", " ", $process_line);
+      $process_line = str_replace("  ", " ", $process_line);
+      $process_line = str_replace("  ", " ", $process_line);
+      $process_line = str_replace("  ", " ", $process_line);
+      $process_line = str_replace("  ", " ", $process_line);
+      $output[$i] = $process_line;
+      $parts = explode(" ", $process_line);
+      if (count($keys) < 1) {
+        $keys = explode(" ", $process_line);
+      }
+    }
+
+    $pid_index = array_search('PID', $keys);
+    $time_index = array_search('TIME', $keys);
+
+    $first_line = TRUE;
+    $run_output = array();
+    foreach ($output as $i => $process_line) {
+      if ($first_line) {
+        $first_line = FALSE;
+      }
+      else {
+        $parts = explode(" ", $process_line);
+
+        $this_pid = $parts[$pid_index];
+        $this_time = $parts[$time_index];
+        @list($h, $m, $s) = explode(":", $this_time);
+        $time_in_seconds = ($h * 3600) + ($m * 60) + $s;
+
+        $convert_index= array_search('/usr/bin/convert', $parts);
+        $this_payload = $parts[($convert_index + 1)];
+        $this_payload = str_replace(
+          array('pitt_', '_OBJ.tif', '/tmp/'),
+          array('pitt:', '', ''),
+          $this_payload);
+
+        // Check to see if this line exceeds the threshold.
+        if ($time_in_seconds > TIME_THRESHOLD) {
+          $markup = kill_pid_and_remove_derivative_job($this_pid, $this_payload, $conn, $time_in_seconds);
+          if ($markup) {
+            $run_output[] = $markup;
+          }
+        }
+      }
+    }
+    @mysqli_close($conn);
+
+    if (count($run_output) > 0) {
+      echo "The CRON job on " . $server_name . " /opt/islandora_cron/kill_pid_after_time.php is killing some convert jobs because they exceeded the threshold of " . TIME_THRESHOLD . " seconds.\n\n";
+      foreach ($run_output as $pid_output) {
+        echo $pid_output;
+      }
+      exit(1);
+    }
+    else {
+      exit(0);
+    }
   }
-  exit(1);
-}
-else {
-  exit(0);
-}
 
 
-function kill_pid_and_remove_job($this_pid, $this_payload, $conn, $time_in_seconds) {
+function kill_pid_and_remove_ocr_job($this_pid, $this_payload, $conn, $time_in_seconds) {
   $ret_markup = '';
 
   // regardless of whether or not the queue record was found, kill this pid.
@@ -106,6 +179,42 @@ function kill_pid_and_remove_job($this_pid, $this_payload, $conn, $time_in_secon
   @list($parent, $sequence) = explode("-", $this_payload);
 
   $ret_markup .= "Killing tesseract job that was running for " . $time_in_seconds . " seconds: \n  " .
+    "pid = " . $this_pid. ", parent = " . $parent . ", payload = " . $this_payload . "\n\n";
+
+  $sql = "SELECT * FROM gearman_queue where data like '%" . $parent . "\"%' AND function_name = 'upitt_workflow_generate_ocr_datastreams'";
+  $ret_markup .= "\n" . $sql . "\n\n";
+  $queue_unique_keys = array();
+  if ($result = $conn->query($sql)) {
+    while ($row = $result->fetch_row()) {
+      if ($row[0] <> '') {
+        $queue_unique_keys[] = $row[0];
+      }
+    }
+    $result->close();
+  }
+  foreach ($queue_unique_keys as $unique_key) {
+    $sql_delete = "DELETE FROM gearman_queue WHERE unique_key = '" . $unique_key . "'";
+    $ret_markup .= "DELETE gearman_queue record: \n  " . $sql_delete . "\n";
+    $conn->query($sql_delete);
+  }
+  if (count($queue_unique_keys) < 1) {
+    $ret_markup .= "no gearman_queue record found matching this object's parent " . $parent . "\n\n";
+  }
+
+  return $ret_markup;
+}
+
+function kill_pid_and_remove_derivative_job($this_pid, $this_payload, $conn, $time_in_seconds) {
+  $ret_markup = '';
+
+  // regardless of whether or not the queue record was found, kill this pid.
+  $ret_markup .= "kill -9 $this_pid\n-------------------------------------------------------\n\n";
+  exec("kill -9 $this_pid");
+
+  // Take the payload value and split the sequence number from it.
+  @list($parent, $sequence) = explode("-", $this_payload);
+
+  $ret_markup .= "Killing convert job that was running for " . $time_in_seconds . " seconds: \n  " .
     "pid = " . $this_pid. ", parent = " . $parent . ", payload = " . $this_payload . "\n\n";
 
   $sql = "SELECT * FROM gearman_queue where data like '%" . $parent . "\"%' AND function_name = 'upitt_workflow_generate_ocr_datastreams'";
@@ -144,4 +253,3 @@ function get_config_value($section,$key) {
     return(0);
   }
 }
-
